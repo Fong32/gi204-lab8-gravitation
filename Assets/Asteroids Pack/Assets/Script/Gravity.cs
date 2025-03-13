d1dfb3e585a7aa7a50107a5fe2ad8048af923a9c
Fong32 <nontarit.arun@bumail.net>
2025-03-13 10:05:18 +0700

    attractScrpit

--- Assets/Asteroids Pack/Assets/Script/Gravity.cs
+++ Assets/Asteroids Pack/Assets/Script/Gravity.cs
@@ -2,5 +2,22 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-    
+
+    Rigidbody rb;
+    const float G = 0.0674f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Attract(Gravity other)
+    {
+        Rigidbody otherRb = other.rb;
+        Vector3 direction = rb.position - otherRb.position;
+        float distance = direction.magnitude;
+        float forceMagnitude = G * (rb.mass * otherRb.mass) /Mathf.Pow(distance, 2);
+        Vector3 finalForce = forceMagnitude * direction.normalized;
+
+        otherRb.AddForce(finalForce);
+    }
 }
